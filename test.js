573e88245eea1a443395c2c0bd0318960871c7a1
netodrt <70249074+netodrt@users.noreply.github.com>
2022-11-14 14:41:47 -0300

    Add files via upload

---
+++ test.js
@@ -0,0 +1,63 @@
+
+
+
+
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = ([{
+  title: 'Desinformação - Todas às segundas-feiras',
+  description: 'Os dois maiores ignorantes deste país, Arthur Petry e Tiago Carvalho, tentam entender as notícias da semana baseados em opiniões estritamente retiradas do CU de ambos.',
+  backgroundImage: 'https://portal.sacocheio.tv/assets/banners/4.svg',
+  linkTo: '/podcast/desinformacao'
+}, {
+  title: 'Tarja Preta FM - Quinzenalmente aos sábados',
+  description: 'Hater número 1 de podcasts no Brasil (incluindo os SEUS), Arthur Petry apresenta um novo formato de programa de humor, nos remetendo aos bons e velhos tempos de rádio dos anos 90.',
+  backgroundImage: 'https://portal.sacocheio.tv/assets/banners/5849.svg',
+  linkTo: '/podcast/tarjaPretaFm'
+}, {
+  title: 'Saco Cheio - Todas às sextas-feiras',
+  description: 'O programa é para ouvintes que não procuram cultura, informação ou opiniões inteligentes. Durante uma hora e meia Arthur Petry reduz o mundo inteiro ao seu próprio umbigo, interpretando tudo de forma egoísta, ignorante e burra.',
+  backgroundImage: 'https://portal.sacocheio.tv/assets/banners/1.svg',
+  linkTo: '/podcast/sacoCheio'
+}, {
+  title: 'Cagando e Andando - Todas às quintas-feiras',
+  description: 'Um programa PAUTADO nos sentimentos de arrependimento e ressentimento. Baseado apenas em intuições infantilizadas, Tiago Carvalho faz observações sobre a existência e explica minuciosamente como as coisas deveriam ser. Além disso, ele conta histórias de sua vida, muitas vezes desastrosas, causadas por medo, procrastinação e PREGUIÇA. Veja em ação o cérebro de alguém que não tem compromisso com rigorosamente nada e se orgulha e se arrepende disso ao mesmo tempo.',
+  backgroundImage: 'https://portal.sacocheio.tv/assets/banners/3.svg',
+  linkTo: '/podcast/cagandoeandando'
+}, {
+  title: 'Uma nova Saco Cheio TV está no ar ❤️',
+  description: 'Estamos felizes em lançar o novo visual da Saco Cheio TV! Conheça mais sobre a Saco Cheio e ajude doando via PIX.',
+  backgroundImage: 'https://portal.sacocheio.tv/assets/banners/default.svg',
+  linkTo: '/sobre'
+}]);
+
+const $ReactRefreshModuleId$ = __webpack_require__.$Refresh$.moduleId;
+const $ReactRefreshCurrentExports$ = __react_refresh_utils__.getModuleExports(
+	$ReactRefreshModuleId$
+);
+
+function $ReactRefreshModuleRuntime$(exports) {
+	if (true) {
+		let errorOverlay;
+		if (true) {
+			errorOverlay = false;
+		}
+		let testMode;
+		if (typeof __react_refresh_test__ !== 'undefined') {
+			testMode = __react_refresh_test__;
+		}
+		return __react_refresh_utils__.executeRuntime(
+			exports,
+			$ReactRefreshModuleId$,
+			module.hot,
+			errorOverlay,
+			testMode
+		);
+	}
+}
+
+if (typeof Promise !== 'undefined' && $ReactRefreshCurrentExports$ instanceof Promise) {
+	$ReactRefreshCurrentExports$.then($ReactRefreshModuleRuntime$);
+} else {
+	$ReactRefreshModuleRuntime$($ReactRefreshCurrentExports$);
+}
+
+/***/ }),
